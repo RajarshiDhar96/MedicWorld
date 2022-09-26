b3ad3360476e614b62a2bad8ac67a68eefce85eb
Rohan Dhar <rajarshid920@gmail.com>
2022-09-27 01:34:46 +0530

    commit

--- client/src/pages/Admin/DoctorsList.js
+++ client/src/pages/Admin/DoctorsList.js
@@ -1,14 +1,80 @@
-import React from 'react'
-import Layout from '../../components/Layout';
+import React, { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux'
+import Layout from '../../components/Layout'
+import { showLoading, hideLoading } from '../../redux/alertsSlice'
+import axios from 'axios'
+import { Table } from 'antd'
 
 function DoctorsList() {
-  return (
-    <Layout>
-      <h1 className="page-header">Doctors List</h1>
-    
-    </Layout>
-  );
-  
+    const [doctors, setDoctors] = useState([])
+    const dispatch = useDispatch();
+    const getUsersData = async () => {
+        try {
+
+            dispatch(showLoading())
+            const response = await axios.get('/api/admin/get-all-doctors', {
+                headers: {
+                    Authorization: `Bearer ${localStorage.getItem('token')}`
+                }
+            })
+            dispatch(hideLoading())
+            if (response.data.success) {
+                setDoctors(response.data.data)
+            }
+        }
+        catch (error) {
+            dispatch(hideLoading())
+
+        }
+
+    }
+
+    useEffect(() => {
+        getUsersData()
+    }, [])
+    const columns = [
+        {
+            title: 'Name',
+            dataIndex: 'name',
+            render: (text, record) =>
+            <span>
+                {record.firstName} {record.lastName}
+            </span>
+        },
+       
+        {
+            title: "Phone",
+            dataIndex: "phoneNumber"
+
+
+        },
+        {
+            title: 'Created At',
+            dataIndex: 'createdAt'
+        },
+        {
+            title: "status",
+            dataIndex: "status"
+        },
+        {
+            title: 'Actions',
+            dataIndex: 'actions',
+            render: (text, record) => (
+                <div className='d-flex'>
+                    {record.status==="Pending" && <h1 className='anchor'>Approve</h1>}
+                    {record.status==="approved" && <h1 className='anchor'>Block</h1>}
+                </div>
+            )
+        },
+    ]
+    return (
+        <Layout>
+            <h1 className="page-header">Doctors List</h1>
+            <Table columns={columns} dataSource={doctors}></Table>
+
+        </Layout>
+    );
+
 }
 
 export default DoctorsList
